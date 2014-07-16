b7bbfb5bf07947d00a7d4b0a3922eb7a40eeb607
Nathan LaFreniere <quitlahok@gmail.com>
2014-07-16 10:39:48 -0700

    initial commit

---
+++ build.js
@@ -0,0 +1,60 @@
+var Fs = require('fs');
+var Async = require('async');
+var Nipple = require('nipple');
+var Cheerio = require('cheerio');
+
+var agents = {};
+
+var parseVersion = function (version) {
+
+    var re = /[^\.]*\.[^\.]*/.exec(version);
+
+    if (re) {
+        return re[0];
+    }
+    else {
+        return version;
+    }
+};
+
+var parse = function (context) {
+
+    var versions = {};
+    var $ = Cheerio.load(context);
+
+    $('h4').each(function (index, header) {
+
+        var version = parseVersion($(header).text().split(' ').pop());
+        versions[version] = [];
+        versions[version].random = function () {
+
+            var randomItem = versions[version][Math.floor(Math.random() * versions[version].length)];
+            if (typeof randomItem === 'function') {
+                return versions[version].random();
+            }
+
+            return randomItem;
+        };
+
+        $('li > a', $(header).next('ul')).each(function (index, str) {
+
+            versions[version].push($(str).text());
+        });
+    });
+
+    return versions;
+};
+
+var browsers = ['Internet Explorer', 'Chrome', 'Firefox', 'Safari', 'Opera'];
+
+Async.each(browsers, function (browser, cb) {
+
+    Nipple.get('http://www.useragentstring.com/pages/' + browser + '/', { }, function (err, res, body) {
+
+        agents[browser] = parse(body);
+        cb();
+    });
+}, function () {
+
+    Fs.writeFileSync('./browsers.json', JSON.stringify(agents), 'utf8');
+});
---
+++ index.js
@@ -0,0 +1,35 @@
+var Browsers = require('./browsers.json');
+
+var pickOne = function (obj) {
+
+    var list = Array.isArray(obj) ? obj : Object.keys(obj);
+    var item = list[Math.floor(Math.random() * list.length)];
+    if (typeof obj[item] === 'function') {
+        return pickOne(list);
+    }
+
+    return item;
+};
+
+Object.keys(Browsers).forEach(function (browser) {
+
+    Object.keys(Browsers[browser]).forEach(function (version) {
+
+        Browsers[browser][version].random = function () {
+
+            return pickOne(Browsers[browser][version]);
+        };
+    });
+
+    Browsers[browser].random = function () {
+
+        return Browsers[browser][pickOne(Browsers[browser])].random();
+    };
+});
+
+Browsers.random = function () {
+
+    return Browsers[pickOne(Browsers)].random();
+};
+
+module.exports = Browsers;
